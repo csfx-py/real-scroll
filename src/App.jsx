5cb2b5915905b251e3ca50fe7723e62b2e6cccc5
csfx-py <csfxshreyas@gmail.com>
2024-11-22 23:28:30 +0530

    init setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+function App() {
+  return (
+    <main className="flex flex-col min-h-screen w-full bg-primary">
+      <div className="h-[200vh]"></div>
+    </main>
+  );
+}
+
+export default App;
