97f52baf3a8047f5aee0cd3e43a2014119763589
csfx-py <csfxshreyas@gmail.com>
2024-12-01 18:00:15 +0530

    marquee changes

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,13 +3,13 @@ export const Marquee = () => {
   return (
     <div className="relative overflow-hidden flex items-center transform -rotate-[5deg]">
       <div
-        className="flex whitespace-nowrap text-white text-xl font-dmsans font-semibold gap-4 w-full"
+        className="flex whitespace-nowrap text-white text-xl font-dmsans font-semibold w-[3952.8px]"
         style={{ animation: "marquee 8s linear infinite" }}
       >
         {Array(20)
           .fill()
           .map((_, index) => (
-            <div key={index} className="flex items-center flex-shrink-0 w-[10%] gap-4">
+            <div key={index} className="flex items-center w-[195px]">
               <img src={ScrollXD} className="w-10" />
               <span className="font-anton text-4xl font-light">$SCROLL</span>
             </div>
